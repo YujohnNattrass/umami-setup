80d4f16b5df5be7b0d5079f041289e1d39a0d5ba
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-11-16 15:40:10 -0800

    Initial commit

---
+++ test/my_ecs_construct.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const MyEcsConstruct = require('../lib/my_ecs_construct-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my_ecs_construct-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new MyEcsConstruct.MyEcsConstructStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
